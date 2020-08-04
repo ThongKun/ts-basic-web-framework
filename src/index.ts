3349388dc1257e366945c70a9d3ce6a80aa32a51
Van Thong <thonglevan131@gmail.com>
2020-08-04 22:45:06 +0700

    implemented get,set User Data( UserProps ); and implemented event functions on, trigger, and implemented fetch and save data through Server(Json Server)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+// const user = new User({});
 
-user.fetch();
-
-setTimeout(() => {
-	console.log(user);
-}, 4000);
+// user.set({ name: 'thong', age: 20 });
+// user.save();
